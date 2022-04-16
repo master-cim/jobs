e067ccaa537d994248e205a675effc9a8f100bde
master-cim <master-cim@yandex.ru>
2022-04-16 16:17:17 +0300

    исправила модел

--- job/mill_decision/admin.py
+++ job/mill_decision/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
+from .models import Posting, Client, Message
 
-# Register your models here.
+
+class PostingAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'start', 'end', 'text', 'client_filter',)
+    search_fields = ('start', 'end',)
+    list_filter = ('client_filter',)
+    empty_value_display = '-пусто-'
+
+
+class ClientAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'phone', 'mobile_code', 'tag', 'timezone',)
+    search_fields = ('phone',)
+    list_filter = ('mobile_code', 'tag',)
+    empty_value_display = '-пусто-'
+
+
+class MessageAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'pub_date', 'sending_status',
+                    'id_posting', 'id_client',)
+    list_editable = ('id_posting', 'id_client',)
+    search_fields = ('pub_date',)
+    list_filter = ('pub_date', 'sending_status',)
+    
+    empty_value_display = '-пусто-'
+
+
+admin.site.register(Posting, PostingAdmin)
+admin.site.register(Client, ClientAdmin)
+admin.site.register(Message, MessageAdmin)
+
--- job/mill_decision/models.py
+++ job/mill_decision/models.py
@@ -1,3 +1,38 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 
-# Create your models here.
+import pytz
+
+
+class Posting(models.Model):
+    start = models.DateTimeField()
+    end = models.DateTimeField()
+    text = models.TextField()
+    client_filter = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.client_filter
+
+
+class Client(models.Model):    
+    phone = models.PositiveIntegerField(
+        validators=[MinValueValidator(70000000000),
+                    MaxValueValidator(79999999999)])
+    mobile_code = models.CharField(max_length=3, blank=False)
+    tag = models.CharField(max_length=50)
+    TIMEZONES = tuple(zip(pytz.all_timezones, pytz.all_timezones))
+    timezone = models.CharField(max_length=32, choices=TIMEZONES,
+                                default='Europe/Moscow')
+
+
+class Message(models.Model):
+    pub_date = models.DateTimeField(auto_now_add=True)
+    sending_status = models.CharField(max_length=250)
+    id_posting = models.ForeignKey(
+        Posting,
+        on_delete=models.CASCADE
+    )
+    id_client = models.ForeignKey(
+        Client,
+        on_delete=models.CASCADE
+    )
