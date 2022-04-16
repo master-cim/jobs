185cc032235b8516fc0c3e3667c1b204583e7448
master-cim <master-cim@yandex.ru>
2022-04-16 09:56:14 +0300

    содано приложение mill_decision

--- job/job/settings.py
+++ job/job/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'mill_decision.apps.MillDecisionConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
